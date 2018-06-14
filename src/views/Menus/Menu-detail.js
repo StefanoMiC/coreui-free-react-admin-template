68c9fb58572e778a7daeaa440046d1f60101e0ce
Unknown <mic@nucleode.com>
2018-06-14 20:09:28 +0200

    adds image grid in menu detail modal

--- src/views/Menus/Menu-detail.js
+++ src/views/Menus/Menu-detail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardHeader, CardImg, Button, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink, Col, Row, Table, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Card, CardBody, CardHeader, CardFooter, CardImg, Button, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink, Col, Row, Table, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class MenuDetail extends Component {
 
@@ -146,13 +146,85 @@ class MenuDetail extends Component {
                         </Row>
                       </Col>
                       <Modal isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
-                        <ModalHeader toggle={this.toggleModal}>Modal title</ModalHeader>
+                        <ModalHeader toggle={this.toggleModal}>Scegli immagine</ModalHeader>
                         <ModalBody>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                          <Row>
+                            <Col xs="12" md="4">
+                              <Card className="cursor" onClick={this.toggleModal}>
+                                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardFooter>
+                                  <i className="icon-doc mr-1"></i><strong>Text</strong>
+                                </CardFooter>
+                              </Card>
+                            </Col>
+                            <Col xs="12" md="4">
+                              <Card className="cursor" onClick={this.toggleModal}>
+                                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardFooter>
+                                  <i className="icon-doc mr-1"></i><strong>Text</strong>
+                                </CardFooter>
+                              </Card>
+                            </Col>
+                            <Col xs="12" md="4">
+                              <Card className="cursor" onClick={this.toggleModal}>
+                                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardFooter>
+                                  <i className="icon-doc mr-1"></i><strong>Text</strong>
+                                </CardFooter>
+                              </Card>
+                            </Col>
+                            <Col xs="12" md="4">
+                              <Card className="cursor" onClick={this.toggleModal}>
+                                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardFooter>
+                                  <i className="icon-doc mr-1"></i><strong>Text</strong>
+                                </CardFooter>
+                              </Card>
+                            </Col>
+                            <Col xs="12" md="4">
+                              <Card className="cursor" onClick={this.toggleModal}>
+                                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardFooter>
+                                  <i className="icon-doc mr-1"></i><strong>Text</strong>
+                                </CardFooter>
+                              </Card>
+                            </Col>
+                            <Col xs="12" md="4">
+                              <Card className="cursor" onClick={this.toggleModal}>
+                                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardFooter>
+                                  <i className="icon-doc mr-1"></i><strong>Text</strong>
+                                </CardFooter>
+                              </Card>
+                            </Col>
+                            <Col xs="12" md="4">
+                              <Card className="cursor" onClick={this.toggleModal}>
+                                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardFooter>
+                                  <i className="icon-doc mr-1"></i><strong>Text</strong>
+                                </CardFooter>
+                              </Card>
+                            </Col>
+                            <Col xs="12" md="4">
+                              <Card className="cursor" onClick={this.toggleModal}>
+                                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardFooter>
+                                  <i className="icon-doc mr-1"></i><strong>Text</strong>
+                                </CardFooter>
+                              </Card>
+                            </Col>
+                            <Col xs="12" md="4">
+                              <Card className="cursor" onClick={this.toggleModal}>
+                                  <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardFooter>
+                                  <i className="icon-doc mr-1"></i><strong>Text</strong>
+                                </CardFooter>
+                              </Card>
+                            </Col>
+                          </Row>
                         </ModalBody>
                         <ModalFooter>
-                          <Button color="primary" onClick={this.toggleModal}>Do Something</Button>{' '}
-                          <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+                          <Button color="secondary" onClick={this.toggleModal}>Indietro</Button>
                         </ModalFooter>
                       </Modal>
                     </Row>
